f66df9902036d11ef62982f432841de255b07278
2jiyong <jdragon2000@naver.com>
2025-06-10 18:38:56 +0900

    feat: 기본적인 엔티티 추가

---
+++ src/main/java/com/dmz/airdnd/accomodation/domain/AccomodationOption.java
@@ -0,0 +1,31 @@
+package com.dmz.airdnd.accomodation.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccomodationOption {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "accomodaton_id", nullable = false)
+	private Accomodation accomodation;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "option_id", nullable = false)
+	private Option option;
+}
