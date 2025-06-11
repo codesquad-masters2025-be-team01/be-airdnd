2013881051830e723c689a647d45b95d46c063de
jang-jinuk <dlsndifn98@gmail.com>
2025-06-11 20:31:32 +0900

    refactor: 이메일 최소 사이즈 변경

--- src/main/java/com/dmz/airdnd/user/dto/request/UserRequest.java
+++ src/main/java/com/dmz/airdnd/user/dto/request/UserRequest.java
@@ -21,7 +21,7 @@ public class UserRequest {
 
 	@NotBlank(message = "이메일은 필수 입력 항목입니다.")
 	@Email(message = "올바른 이메일 형식이어야 합니다.")
-	@Size(min = 5, max = 25, message = "이메일은 5~25자 이내여야 합니다.")
+	@Size(min = 6, max = 25, message = "이메일은 6~25자 이내여야 합니다.")
 	private String email;
 
 	@NotBlank(message = "전화번호는 필수 입력 항목입니다.")
