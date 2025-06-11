2949a647836d31292710f1c3b67b5c47742f3afb
2jiyong <jdragon2000@naver.com>
2025-06-11 12:21:54 +0900

    test: 실패 테스트 추가

--- src/test/java/com/dmz/airdnd/common/auth/AuthServiceTest.java
+++ src/test/java/com/dmz/airdnd/common/auth/AuthServiceTest.java
@@ -3,9 +3,14 @@ package com.dmz.airdnd.common.auth;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.mockito.Mockito.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -57,6 +62,47 @@ class AuthServiceTest {
 			.build();
 	}
 
+	@ParameterizedTest
+	@MethodSource("provideDuplicateRequests")
+	@DisplayName("중복된 필드가 있을 경우 회원가입에 실패한다.")
+	void duplicateFieldTest(UserRequest request, String duplicateField) {
+		//given
+		switch (duplicateField) {
+			case "loginId" -> {
+				when(userRepository.existsByLoginId(request.getLoginId())).thenReturn(true);
+			}
+			case "email" -> {
+				when(userRepository.existsByLoginId(request.getLoginId())).thenReturn(false);
+				when(userRepository.existsByEmail(request.getEmail())).thenReturn(true);
+			}
+			case "phone" -> {
+				when(userRepository.existsByLoginId(request.getLoginId())).thenReturn(false);
+				when(userRepository.existsByEmail(request.getEmail())).thenReturn(false);
+				when(userRepository.existsByPhone(request.getPhone())).thenReturn(true);
+			}
+		}
+		//when + then
+		switch (duplicateField) {
+			case "loginId" -> assertDuplicateError(() -> authService.signup(request), "이미 존재하는 로그인 아이디입니다.");
+			case "email" -> assertDuplicateError(() -> authService.signup(request), "이미 존재하는 이메일입니다.");
+			case "phone" -> assertDuplicateError(() -> authService.signup(request), "이미 존재하는 전화번호입니다.");
+		}
+	}
+
+	private void assertDuplicateError(Runnable executable, String expectedMessage) {
+		assertThatThrownBy(executable::run)
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(expectedMessage);
+	}
+
+	private static Stream<Arguments> provideDuplicateRequests() {
+		return Stream.of(
+			Arguments.of(new UserRequest("test123", "pw", "new@example.com", "010-9999-8888"), "loginId"), // loginId 중복
+			Arguments.of(new UserRequest("newId", "pw", "test@example.com", "010-9999-8888"), "email"),   // email 중복
+			Arguments.of(new UserRequest("newId", "pw", "new@example.com", "010-1234-5678"), "phone")    // phone 중복
+		);
+	}
+
 	private UserRequest createUserRequestFrom(User user) {
 		return UserRequest.builder()
 			.loginId(user.getLoginId())
@@ -65,5 +111,4 @@ class AuthServiceTest {
 			.phone(user.getPhone())
 			.build();
 	}
-
 }
