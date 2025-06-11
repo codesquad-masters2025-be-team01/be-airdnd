984b04dc9e886f85ccd0c8b1a9db069af6cd0592
2jiyong <jdragon2000@naver.com>
2025-06-11 18:47:55 +0900

    refactor: 예외 메시지 추가

--- src/main/java/com/dmz/airdnd/user/dto/request/response/UserRequest.java
+++ src/main/java/com/dmz/airdnd/user/dto/request/response/UserRequest.java
@@ -1,7 +1,7 @@
 package com.dmz.airdnd.user.dto.request.response;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,20 +11,20 @@ import lombok.Getter;
 @Builder
 @AllArgsConstructor
 public class UserRequest {
-	@NotEmpty
-	@Size(min = 5, max = 25)
+	@NotBlank(message = "로그인 아이디는 필수 입력 항목입니다.")
+	@Size(min = 5, max = 25, message = "로그인 아이디는 5~25자 이내여야 합니다.")
 	private String loginId;
 
-	@NotEmpty
-	@Size(min = 5, max = 25)
+	@NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+	@Size(min = 5, max = 25, message = "비밀번호는 5~25자 이내여야 합니다.")
 	private String password;
 
-	@NotEmpty
-	@Email
-	@Size(min = 5, max = 25)
+	@NotBlank(message = "이메일은 필수 입력 항목입니다.")
+	@Email(message = "올바른 이메일 형식이어야 합니다.")
+	@Size(min = 5, max = 25, message = "이메일은 5~25자 이내여야 합니다.")
 	private String email;
 
-	@NotEmpty
-	@Size(min = 8, max = 25)
+	@NotBlank(message = "전화번호는 필수 입력 항목입니다.")
+	@Size(min = 8, max = 25, message = "전화번호는 8~25자 이내여야 합니다.")
 	private String phone;
 }
