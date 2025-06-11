d54e20a65419961a9dc45e2ea8fccd7b066b7015
2jiyong <jdragon2000@naver.com>
2025-06-11 12:03:45 +0900

    refactor: Mapper 클래스 추가

--- src/main/java/com/dmz/airdnd/common/auth/AuthService.java
+++ src/main/java/com/dmz/airdnd/common/auth/AuthService.java
@@ -3,9 +3,9 @@ package com.dmz.airdnd.common.auth;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.dmz.airdnd.user.domain.Role;
 import com.dmz.airdnd.user.domain.User;
 import com.dmz.airdnd.user.dto.request.response.UserRequest;
+import com.dmz.airdnd.user.mapper.UserMapper;
 import com.dmz.airdnd.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -27,13 +27,7 @@ public class AuthService {
 			throw new IllegalArgumentException("이미 존재하는 전화번호입니다.");
 		}
 
-		User user = User.builder()
-			.loginId(userRequest.getLoginId())
-			.password(userRequest.getPassword())
-			.email(userRequest.getEmail())
-			.phone(userRequest.getPhone())
-			.role(Role.USER)
-			.build();
+		User user = UserMapper.toEntity(userRequest);
 
 		return userRepository.save(user);
 	}
