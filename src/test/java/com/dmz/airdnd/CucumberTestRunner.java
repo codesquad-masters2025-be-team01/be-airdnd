d4884f3886445259189797fcd5953bde695624a9
2jiyong <jdragon2000@naver.com>
2025-06-12 14:47:34 +0900

    test: cucumber stepdef 테스트추가

--- src/test/java/com/dmz/airdnd/CucumberTestRunner.java
+++ src/test/java/com/dmz/airdnd/CucumberTestRunner.java
@@ -11,10 +11,9 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:build/reports/cucumber.html, summary")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefinitions")
-public class CucumberTestRunner {
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.dmz.airdnd.stepdefinitions")
+public class CucumberTestRunner extends AbstractContainerBase {
 	/*
 	Cucumber 테스트 러너
-	빈 클래스이지만 @Cucumber 어노테이션으로 테스트 엔트리포인트 역할
 	 */
 }
---
+++ src/test/java/com/dmz/airdnd/stepdefinitions/SignUpStepDef.java
@@ -0,0 +1,77 @@
+package com.dmz.airdnd.stepdefinitions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.dmz.airdnd.common.auth.AuthService;
+import com.dmz.airdnd.user.dto.request.UserRequest;
+import com.dmz.airdnd.user.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@CucumberContextConfiguration
+public class SignUpStepDef {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private AuthService authService;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	private UserRequest request;
+	private ResultActions resultActions;
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Given("유효한 회원가입 요청 정보가 준비되어 있다.")
+	public void 유효한_회원가입_요청_정보_준비되어_있다() {
+		request = new UserRequest(
+			"user123", "pass!234", "user@example.com", "01012345678"
+		);
+	}
+
+	@Given("저장소에 동일한 loginId, email, phone을 가진 유저가 존재하지 않는다.")
+	public void 저장소에_동일한_유저가_존재하지_않는다() {
+	}
+
+	@When("사용자가 회원가입 API를 호출했을 때")
+	public void 사용자가_회원가입_API를_호출하면() throws Exception {
+		String json = objectMapper.writeValueAsString(request);
+		resultActions = mockMvc.perform(post("/api/auth/signup")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(json));
+	}
+
+	@Then("응답 상태로 201 Created를 받는다.")
+	public void 회원가입_반환_정보_비교() throws Exception {
+		resultActions.andExpect(status().isCreated());
+	}
+
+	@Then("응답 본문은 다음과 같아야 한다:")
+	public void 응답_본문은_다음과_같아야_한다(DataTable dataTable) throws Exception {
+		Map<String, String> expected = dataTable.asMaps().get(0);
+		resultActions
+			.andExpect(jsonPath("$.success").value(Boolean.parseBoolean(expected.get("success"))))
+			.andExpect(jsonPath("$.data").isEmpty())
+			.andExpect(jsonPath("$.error").isEmpty());
+	}
+}
