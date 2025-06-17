8f0033451bddc3a24b2b9d5d167385d3ca3cef5c
2jiyong <jdragon2000@naver.com>
2025-06-17 14:10:48 +0900

    refactor: onetoone으로 관계 변경 후 cascadetype all 추가

--- src/main/java/com/dmz/airdnd/accommodation/domain/Accommodation.java
+++ src/main/java/com/dmz/airdnd/accommodation/domain/Accommodation.java
@@ -2,13 +2,15 @@ package com.dmz.airdnd.accommodation.domain;
 
 import java.sql.Timestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,7 +26,7 @@ public class Accommodation {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne(optional = false)
+	@OneToOne(optional = false, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@JoinColumn(name = "address_id", nullable = false)
 	private Address address;
 
