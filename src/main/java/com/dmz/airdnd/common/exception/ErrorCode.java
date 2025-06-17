b76638a413f26445de0894ca2bcc547b199e39e6
jang-jinuk <dlsndifn98@gmail.com>
2025-06-17 22:41:26 +0900

    feat: 중복 예약 예외 추가

--- src/main/java/com/dmz/airdnd/common/exception/ErrorCode.java
+++ src/main/java/com/dmz/airdnd/common/exception/ErrorCode.java
@@ -28,6 +28,8 @@ public enum ErrorCode {
 	PAYLOAD_TOO_LARGE(413, "PAYLOAD_TOO_LARGE", "파일크기가 혀용된 최대 크기를 초과하였습니다."),
 	// 인가 관련
 	FORBIDDEN(403, "FORBIDDEN", "해당 작업을 수행할 권한이 없습니다."),
+	// 예약 관련
+	DUPLICATE_RESERVATION(409, "DUPLICATE_RESERVATION", "이미 존재하는 예약입니다."),
 	// Google API 관련
 	GEOCODING_FAILED(502, "GEOCODING_FAILED", "주소 정보를 가져오는 데 실패했습니다."),
 	// 숙소 관련
