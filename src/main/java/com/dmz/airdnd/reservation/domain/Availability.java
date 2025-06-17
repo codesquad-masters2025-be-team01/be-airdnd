07983b0c5e4592d7095329e72cadb9becf7e25db
jang-jinuk <dlsndifn98@gmail.com>
2025-06-17 16:45:22 +0900

    feat: date 필드 unique 제약 조건 추가

--- src/main/java/com/dmz/airdnd/reservation/domain/Availability.java
+++ src/main/java/com/dmz/airdnd/reservation/domain/Availability.java
@@ -26,7 +26,7 @@ public class Availability {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private LocalDate date;
 
 	@ManyToOne(optional = false)
