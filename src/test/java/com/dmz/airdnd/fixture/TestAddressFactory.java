f8deed02b3b31c066024fe0568f9b09108b46d5f
jang-jinuk <dlsndifn98@gmail.com>
2025-06-17 12:52:49 +0900

    refactor: Address 엔티티 필드 값 변경으로 인한 팩토리 수정

--- src/test/java/com/dmz/airdnd/fixture/TestAddressFactory.java
+++ src/test/java/com/dmz/airdnd/fixture/TestAddressFactory.java
@@ -1,30 +1,37 @@
 package com.dmz.airdnd.fixture;
 
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+
 import com.dmz.airdnd.accommodation.domain.Address;
 
 public class TestAddressFactory {
+
+	private static final GeometryFactory geometryFactory = new GeometryFactory();
+
 	public static Address createTestAddress() {
+		Point location = geometryFactory.createPoint(new Coordinate(127.123456, 37.123456)); // 경도, 위도 순서
+		location.setSRID(4326);
+
 		return Address.builder()
 			.country("대한민국")
-			.city("제주도")
-			.state("서울특별시")
-			.postalCode("12345")
-			.addressLine1("강남구 테헤란로 123")
-			.addressLine2("ABC 오피스텔 101호")
-			.location("37.123456,127.123456")
+			.baseAddress("서울특별시 강남구 테헤란로 123")
+			.detailedAddress("ABC 오피스텔 101호")
+			.location(location)
 			.build();
 	}
 
 	public static Address createTestAddress(Long id) {
+		Point location = geometryFactory.createPoint(new Coordinate(127.123456, 37.123456));
+		location.setSRID(4326);
+
 		return Address.builder()
 			.id(id)
 			.country("대한민국")
-			.city("제주도")
-			.state("서울특별시")
-			.postalCode("12345")
-			.addressLine1("강남구 테헤란로 123")
-			.addressLine2("ABC 오피스텔 101호")
-			.location("37.123456,127.123456")
+			.baseAddress("서울특별시 강남구 테헤란로 123")
+			.detailedAddress("ABC 오피스텔 101호")
+			.location(location)
 			.build();
 	}
 }
