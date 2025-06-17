e3ea4c59f438b6b07f61e26f4074fdd5f6ad67d2
jang-jinuk <dlsndifn98@gmail.com>
2025-06-17 14:39:09 +0900

    refactor: Availability 팩토리 구조변경

--- src/test/java/com/dmz/airdnd/fixture/TestAvailabilityFactory.java
+++ src/test/java/com/dmz/airdnd/fixture/TestAvailabilityFactory.java
@@ -1,15 +1,16 @@
 package com.dmz.airdnd.fixture;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import com.dmz.airdnd.accommodation.domain.Accommodation;
 import com.dmz.airdnd.reservation.domain.Availability;
 import com.dmz.airdnd.reservation.domain.Reservation;
 
 public class TestAvailabilityFactory {
-	public static Availability createTestAvailability(Accommodation accommodation, Reservation reservation) {
+	public static Availability createTestAvailability(Accommodation accommodation, Reservation reservation,
+		LocalDate date) {
 		return Availability.builder()
-			.date(new Date())
+			.date(date)
 			.accommodation(accommodation)
 			.reservation(reservation)
 			.build();
