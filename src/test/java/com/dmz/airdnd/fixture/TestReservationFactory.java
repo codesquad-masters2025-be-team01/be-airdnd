44f67d8ebb069df3d418bc8bb203ea3472a9e931
jang-jinuk <dlsndifn98@gmail.com>
2025-06-17 11:00:33 +0900

    test: TestReservationFactory 구조 변경으로 인한 테스트 코드 수정

--- src/test/java/com/dmz/airdnd/fixture/TestReservationFactory.java
+++ src/test/java/com/dmz/airdnd/fixture/TestReservationFactory.java
@@ -3,16 +3,14 @@ package com.dmz.airdnd.fixture;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-import com.dmz.airdnd.accommodation.domain.Accommodation;
 import com.dmz.airdnd.reservation.domain.Reservation;
 import com.dmz.airdnd.reservation.domain.ReservationStatus;
-import com.dmz.airdnd.user.domain.User;
 
 public class TestReservationFactory {
-	public static Reservation createTestReservation(Accommodation accommodation, User guest) {
+	public static Reservation createTestReservation() {
 		return Reservation.builder()
-			.guest(guest)
-			.accommodation(accommodation)
+			.guest(TestUserFactory.createTestUser(1L))
+			.accommodation(TestAccommodationFactory.createTestAccommodation(1L))
 			.checkInDate(new Date())
 			.checkOutDate(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24))  // 1일 후
 			.numberOfGuests(10)
--- src/test/java/com/dmz/airdnd/reservation/repository/ReservationRepositoryTest.java
+++ src/test/java/com/dmz/airdnd/reservation/repository/ReservationRepositoryTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.dmz.airdnd.AbstractContainerBase;
 import com.dmz.airdnd.accommodation.domain.Accommodation;
-import com.dmz.airdnd.accommodation.domain.Address;
 import com.dmz.airdnd.accommodation.domain.repository.AccommodationRepository;
 import com.dmz.airdnd.accommodation.domain.repository.AddressRepository;
 import com.dmz.airdnd.fixture.TestAccommodationFactory;
@@ -43,16 +42,15 @@ class ReservationRepositoryTest extends AbstractContainerBase {
 	@BeforeEach
 	void setup() {
 		guest = userRepository.save(TestUserFactory.createTestUser());
-
-		Address address = addressRepository.save(TestAddressFactory.createTestAddress());
-		accommodation = accommodationRepository.save(TestAccommodationFactory.createTestAccommodation(address));
+		addressRepository.save(TestAddressFactory.createTestAddress());
+		accommodation = accommodationRepository.save(TestAccommodationFactory.createTestAccommodation());
 	}
 
 	@Test
 	@DisplayName("예약을 등록할 수 있다.")
 	void success_saveReservation() {
 		//given
-		Reservation reservation = TestReservationFactory.createTestReservation(accommodation, guest);
+		Reservation reservation = TestReservationFactory.createTestReservation();
 
 		//when
 		Reservation newReservation = reservationRepository.save(reservation);
