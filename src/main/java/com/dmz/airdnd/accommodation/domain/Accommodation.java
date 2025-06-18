d6004eba747ceca5655941db3b0e0ebd44e13fe6
2jiyong <jdragon2000@naver.com>
2025-06-18 14:02:33 +0900

    refactor: ManyToMany로 변경하고 중간 엔티티 제거

--- src/main/java/com/dmz/airdnd/accommodation/domain/Accommodation.java
+++ src/main/java/com/dmz/airdnd/accommodation/domain/Accommodation.java
@@ -1,6 +1,7 @@
 package com.dmz.airdnd.accommodation.domain;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +11,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,6 +33,14 @@ public class Accommodation {
 	@OneToOne(optional = false, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@JoinColumn(name = "address_id", nullable = false)
 	private Address address;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(
+		name = "accommodation_label",
+		joinColumns = @JoinColumn(name = "accommodation_id"),
+		inverseJoinColumns = @JoinColumn(name = "label_id")
+	)
+	private List<Label> labels;
 
 	@Column(nullable = false, length = 50)
 	private String name;
@@ -60,4 +71,3 @@ public class Accommodation {
 
 	private Timestamp updatedAt;
 }
-
