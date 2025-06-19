e1efe6dcb70da45b7de13dde244e5ff3ce2b6525
doopang24 <wodus4145@naver.com>
2025-06-19 10:39:34 +0900

    feat: 숙소 생성 요청 dto에서 Accommodation 엔티티로 매핑해주는 mapper 생성

--- src/main/java/com/dmz/airdnd/accommodation/mapper/AccommodationMapper.java
+++ src/main/java/com/dmz/airdnd/accommodation/mapper/AccommodationMapper.java
@@ -4,12 +4,27 @@ import java.util.List;
 
 import com.dmz.airdnd.accommodation.domain.Accommodation;
 import com.dmz.airdnd.accommodation.domain.Address;
+import com.dmz.airdnd.accommodation.dto.request.AccommodationCreateRequest;
 import com.dmz.airdnd.accommodation.domain.Label;
 import com.dmz.airdnd.accommodation.dto.response.AccommodationResponse;
 import com.dmz.airdnd.accommodation.dto.response.AddressResponse;
 import com.dmz.airdnd.accommodation.dto.response.LabelResponse;
 
 public class AccommodationMapper {
+	public static Accommodation toEntity(AccommodationCreateRequest request, Address address) {
+		return Accommodation.builder()
+			.name(request.getName())
+			.description(request.getDescription())
+			.pricePerDay(request.getPricePerDay())
+			.currency(request.getCurrency())
+			.maxGuests(request.getMaxGuests())
+			.bedCount(request.getBedCount())
+			.bedroomCount(request.getBedroomCount())
+			.bathroomCount(request.getBathroomCount())
+			.address(address)
+			.build();
+	}
+
 	public static AccommodationResponse toResponse(Accommodation accommodation) {
 		return AccommodationResponse.builder()
 			.id(accommodation.getId())
