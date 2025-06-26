533db3b68ea9e7664b187eeb09dccd65f1908651
doopang24 <wodus4145@naver.com>
2025-06-26 19:02:47 +0900

    feat: reservation을 엘라스틱서치에 색인하기 위한 코드 추가

--- src/main/java/com/dmz/airdnd/accommodation/document/AccommodationDocument.java
+++ src/main/java/com/dmz/airdnd/accommodation/document/AccommodationDocument.java
@@ -1,9 +1,11 @@
 package com.dmz.airdnd.accommodation.document;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -16,57 +18,63 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@Builder(toBuilder = true)
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(force = true)
 @Document(indexName = "accommodations")
 public class AccommodationDocument {
 
 	@Id
-	private String id;
+	private final String id;
 
 	@Field(type = FieldType.Text)
-	private String name;
+	private final String name;
 
 	@Field(type = FieldType.Text)
-	private String description;
+	private final String description;
 
 	@Field(type = FieldType.Long)
-	private Long pricePerDay;
+	private final Long pricePerDay;
 
 	@Field(type = FieldType.Keyword)
-	private String currency;
+	private final String currency;
 
 	@Field(type = FieldType.Integer)
-	private Integer maxGuests;
+	private final Integer maxGuests;
 
 	@Field(type = FieldType.Integer)
-	private Integer bedCount;
+	private final Integer bedCount;
 
 	@Field(type = FieldType.Integer)
-	private Integer bedroomCount;
+	private final Integer bedroomCount;
 
 	@Field(type = FieldType.Integer)
-	private Integer bathroomCount;
+	private final Integer bathroomCount;
 
 	@Field(type = FieldType.Date)
-	private Instant createdAt;
+	private final Instant createdAt;
 
 	@Field(type = FieldType.Date)
-	private Instant updatedAt;
+	private final Instant updatedAt;
 
 	@GeoPointField
-	private Point location;
+	private final Point location;
 
 	@Field(type = FieldType.Keyword)
-	private String country;
+	private final String country;
 
 	@Field(type = FieldType.Text)
-	private String baseAddress;
+	private final String baseAddress;
 
 	@Field(type = FieldType.Text)
-	private String detailedAddress;
+	private final String detailedAddress;
 
 	@Field(type = FieldType.Keyword)
-	private List<String> labels;
+	private final List<String> labels;
+
+	@Field(type = FieldType.Date, format = DateFormat.date)
+	private final List<LocalDate> availableDates;
+
+	@Field(type = FieldType.Nested)
+	private final List<ReservationInfo> reservations;
 }
